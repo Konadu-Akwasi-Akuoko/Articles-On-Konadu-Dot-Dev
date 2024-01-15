958bca298a73bf62ac86c99ffc0383300b601943
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2024-01-15 13:51:24 +0000

    Delete August_2023/how-to-use-typescript-type-guard directory

--- August_2023/how-to-use-typescript-type-guard/typeguard.ts
+++
@@ -1,9 +0,0 @@
-function format(param: string | number) {
-  if (typeof param === "string") {
-    // TypeScript knows that param is a string here
-    return param.toUpperCase();
-  } else {
-    // TypeScript knows that param is a number here
-    return param.toFixed(2);
-  }
-}
